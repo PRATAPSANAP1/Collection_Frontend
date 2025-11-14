a800ce8249e35b8ca4923beb5570d0dcd5a1f388
PRATAPSANAP1 <pratapsanap14@gmail.com>
2025-11-14 23:39:12 +0530

    Initial frontend setup for deployment

---
+++ config.js
@@ -0,0 +1,11 @@
+const isLocalhost =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
+
+console.log("hello host : ",isLocalhost);
+const API_BASE_URL = isLocalhost
+  ? "http://localhost:3400" // local backend
+  : "https://shubhangi-collection-backend.onrender.com"; // hosted backend
+
+// make it globally accessible
+window.Backend_URL = API_BASE_URL;
